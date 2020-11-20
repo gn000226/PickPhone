e1bd0536599bd4442e03d349dc22182f12eee020
xuan <gn0987123@yahoo.com.tw>
2020-11-20 19:56:59 +0800

    peter_homework_01

---
+++ PickPhone/PhoneCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhoneCollectionViewCell.swift
+//  PickPhone
+//
+//  Created by 陳暘璿 on 2020/11/19.
+//
+
+import UIKit
+
+class PhoneCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var pickColorView: UIView!
+    
+}
